5c6d9b812dd872a7b301429aecda3b15f2cef612
Alexander Krivonosov <akrivonosov@provectus.com>
2022-02-10 19:48:40 +0300

    set up jest

---
+++ src/App.spec.tsx
@@ -0,0 +1,9 @@
+import App from './App';
+import {render} from '@testing-library/react'
+
+describe('App', () => {
+  it('matches the snapshot', () => {
+    const wrapper = render(<App/>);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  })
+})
