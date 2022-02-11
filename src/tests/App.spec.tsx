2a6f019e11514f5893e681c613074bcfd5a842d7
Alexander Krivonosov <akrivonosov@provectus.com>
2022-02-11 11:57:59 +0300

    fix tests

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,9 +1,10 @@
 import App from '../App';
 import {render} from '@testing-library/react'
+import {BrowserRouter} from 'react-router-dom'
 
 describe('App', () => {
   it('matches the snapshot', () => {
-    const wrapper = render(<App/>);
+    const wrapper = render(<BrowserRouter><App/></BrowserRouter>);
     expect(wrapper.baseElement).toMatchSnapshot();
   })
 })
